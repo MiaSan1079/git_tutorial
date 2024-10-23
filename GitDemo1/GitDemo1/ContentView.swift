006122ecc2352e0547de3e8c09a552d4c1b190b1
MiaSan1079 <ruta.10021996@gmail.com>
2024-10-23 16:25:06 +0900

    initial commit

---
+++ GitDemo1/GitDemo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitDemo1
+//
+//  Created by 齋藤龍太 on 2024/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitDemo1/GitDemo1/GitDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemo1App.swift
+//  GitDemo1
+//
+//  Created by 齋藤龍太 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct GitDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
